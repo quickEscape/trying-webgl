1687422145a8a974670125ee02d55a266602697d
Evgeniy Kononko <evgeniy.escape@gmail.com>
2019-06-10 14:52:54 +0300

    add helper classes

---
+++ src/js/modules/StringToNumberHelper.js
@@ -0,0 +1,12 @@
+export default class StringToNumberHelper {
+  constructor(obj, prop) {
+    this.obj = obj;
+    this.prop = prop;
+  }
+  get value() {
+    return this.obj[this.prop];
+  }
+  set value(v) {
+    this.obj[this.prop] = parseFloat(v);
+  }
+}
